c9ad9be9506a84867a31d9a2f4995b00d18a790a
IgnacioTB16 <TBIgnacio16@outlook.com>
2025-01-30 16:53:30 -0500

    modificacion en la cabecera e InicioSesion

--- proyectomenug7/src/app/components/InicioSesion/InicioSesion.component.ts
+++ proyectomenug7/src/app/components/InicioSesion/InicioSesion.component.ts
@@ -1,4 +1,5 @@
 import { Component, } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -13,8 +14,28 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    FormsModule
   ],
   templateUrl: './InicioSesion.component.html',
   styleUrls: ['./InicioSesion.component.css'],
 })
-export class InicioSesionComponent {}
+
+export class InicioSesionComponent {
+  email: string = '';
+  password: string = '';
+
+  users = [
+    {email: 'Pepito@ug.com', password: 'student', role: 'estudiante'},
+    {email: 'Juanito@ug.com', password: 'student', role: 'estudiante'},
+    {email: 'Maria@ug.com', password: 'secretary', role: 'secretaria'},
+  ]
+  constructor(private router: Router) {}
+  IniciarSesion() {
+    const user = this.users.find(u => u.email === this.email && u.password === this.password);
+    if (user) {
+      this.router.navigate(['/home']);
+    } else {
+      alert('Usuario y/o contraseña incorrectos');
+    }
+  }
+}
